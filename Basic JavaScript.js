dbcdf371dfb809b18b4511e40096f6233b0601bd
metalheadpnz <metalheadpnz@gmail.com>
2021-12-21 23:23:20 +0300

    daily codeCamp

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -11,3 +11,33 @@ a = 7;
 var b;
 b=a;
 // Only change code below this line
+
+// Only change code below this line
+var a =5;
+var b =10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+//===
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//===
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//===
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
