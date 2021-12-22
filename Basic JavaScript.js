2c168cd3ec18abc624ded69be21355adfacb0947
metalheadpnz <metalheadpnz@gmail.com>
2021-12-22 19:15:04 +0300

    daily codeCamp 22_12_21

--- Basic JavaScript.js
+++ Basic JavaScript.js
@@ -50,3 +50,65 @@ const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
 // Only change code below this line
 const myName = "Andr";
 const myStr = 'My name is ' + myName +'and I am well!';
+
+//==============================================================
+//Use Bracket Notation to Find the First Character in a String
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+//==============================================================
+//Understand String Immutability
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+
+//==============================================================
+//Use Bracket Notation to Find the Nth Character in a String
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+//==============================================================
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
+
+//==============================================================
+
+//==============================================================
+
+//==============================================================
+
+//==============================================================
+
+//==============================================================
+
+//==============================================================
+
+//==============================================================
+
+//==============================================================
+
+//==============================================================
+
+//==============================================================
+
+//==============================================================
+
+//==============================================================
+
+//==============================================================
+
+//==============================================================
